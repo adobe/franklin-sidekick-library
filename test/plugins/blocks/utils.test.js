b4a2acd93383362a88e508add01eb377a232e211
dylandepass <dylandepass@gmail.com>
2023-12-22 11:23:46 -0500

    feat: support for custom table header colors (#99)
    * feat: support for custom table header colors
    
    * fix: flaky test
    
    * fix: flaky test
    
    * fix: flaky test
    
    * fix: flaky test
    
    * fix: flaky test
    
    * fix: flaky test
    
    * fix: bug
    
    * fix: readme update
    
    * fix: tests
    
    * Update src/plugins/blocks/utils.js
    
    Co-authored-by: Raphael Wegmueller <github@rofe.com>
    
    * Update src/plugins/blocks/utils.js
    
    Co-authored-by: Raphael Wegmueller <github@rofe.com>
    
    ---------
    
    Co-authored-by: Raphael Wegmueller <github@rofe.com>

--- test/plugins/blocks/utils.test.js
+++ test/plugins/blocks/utils.test.js
@@ -13,7 +13,14 @@
 /* eslint-disable no-unused-expressions */
 
 import { expect } from '@open-wc/testing';
-import { getBlockName, normalizeBlockName, convertBlockToTable } from '../../../src/plugins/blocks/utils.js';
+import {
+  getBlockName,
+  normalizeBlockName,
+  convertBlockToTable,
+  getBlockTableStyle,
+  getPreferedBackgroundColor,
+  getPreferedForegroundColor,
+} from '../../../src/plugins/blocks/utils.js';
 import { mockBlock } from '../../fixtures/blocks.js';
 import { CARDS_DEFAULT_STUB, CARDS_WITH_ALIGNMENT_STUB } from '../../fixtures/stubs/cards.js';
 
@@ -45,10 +52,10 @@ describe('Blocks Util', () => {
   });
   describe('normalizeBlockName()', () => {
     it('returns author friendly names', async () => {
-      expect(normalizeBlockName('hero-main')).to.equal('hero main');
-      expect(normalizeBlockName('hero-main (layer-1)')).to.equal('hero main (layer 1)');
-      expect(normalizeBlockName('hero-main (layer-1, bold-italic)')).to.equal('hero main (layer 1, bold italic)');
-      expect(normalizeBlockName('hero-main-foo-bar (layer-1, bold-italic, underline)')).to.equal('hero main foo bar (layer 1, bold italic, underline)');
+      expect(normalizeBlockName('hero-main')).to.equal('Hero Main');
+      expect(normalizeBlockName('hero-main (layer-1)')).to.equal('Hero Main (layer 1)');
+      expect(normalizeBlockName('hero-main (layer-1, bold-italic)')).to.equal('Hero Main (layer 1, bold italic)');
+      expect(normalizeBlockName('hero-main-foo-bar (layer-1, bold-italic, underline)')).to.equal('Hero Main Foo Bar (layer 1, bold italic, underline)');
     });
   });
 
@@ -67,6 +74,110 @@ describe('Blocks Util', () => {
       const thirdDataRow = table.querySelector('tr:nth-of-type(4)');
       expect(thirdDataRow.querySelector('td:first-of-type').getAttribute('data-valign')).to.equal('middle');
       expect(thirdDataRow.querySelector('td:nth-of-type(2)').getAttribute('data-valign')).to.equal('middle');
+    });
+  });
+
+  describe('convertBlockToTable()', () => {
+    it('should preserve data-align & data-valign', async () => {
+      const cardsBlock = mockBlock(CARDS_WITH_ALIGNMENT_STUB);
+      const table = await convertBlockToTable({}, cardsBlock, 'cards', 'https://localhost:3000');
+      const firstDataRow = table.querySelector('tr:nth-of-type(2)');
+      expect(firstDataRow.querySelector('td:first-of-type').getAttribute('data-align')).to.equal('center');
+      expect(firstDataRow.querySelector('td:first-of-type').getAttribute('data-valign')).to.equal('middle');
+
+      const secondDataRow = table.querySelector('tr:nth-of-type(3)');
+      expect(secondDataRow.querySelector('td:first-of-type').getAttribute('data-align')).to.equal('center');
+      expect(secondDataRow.querySelector('td:nth-of-type(2)').getAttribute('data-align')).to.equal('center');
+
+      const thirdDataRow = table.querySelector('tr:nth-of-type(4)');
+      expect(thirdDataRow.querySelector('td:first-of-type').getAttribute('data-valign')).to.equal('middle');
+      expect(thirdDataRow.querySelector('td:nth-of-type(2)').getAttribute('data-valign')).to.equal('middle');
+    });
+  });
+
+  describe('getBlockTableStyle', () => {
+    it('prefers section library metadata over default library metadata', () => {
+      const defaultMetadata = {
+        tableheaderbackgroundcolor: 'red',
+        tableheaderforegroundcolor: 'blue',
+      };
+      const sectionMetadata = {
+        tableheaderbackgroundcolor: 'green',
+        tableheaderforegroundcolor: 'yellow',
+      };
+      const result = getBlockTableStyle(defaultMetadata, sectionMetadata);
+      expect(result).to.deep.equal({
+        tableHeaderBackgroundColor: 'green',
+        tableHeaderForegroundColor: 'yellow',
+      });
+    });
+
+    it('falls back to default library metadata if section library metadata is missing', () => {
+      const defaultMetadata = {
+        tableheaderbackgroundcolor: 'red',
+        tableheaderforegroundcolor: 'blue',
+      };
+      const sectionMetadata = {};
+      const result = getBlockTableStyle(defaultMetadata, sectionMetadata);
+      expect(result).to.deep.equal({
+        tableHeaderBackgroundColor: 'red',
+        tableHeaderForegroundColor: 'blue',
+      });
+    });
+
+    it('returns an empty object if both metadata objects are missing the properties', () => {
+      const defaultMetadata = {};
+      const sectionMetadata = {};
+      const result = getBlockTableStyle(defaultMetadata, sectionMetadata);
+      expect(result).to.be.empty;
+    });
+  });
+
+  describe('getPreferedBackgroundColor', () => {
+    it('returns the correct color for "section metadata" block', () => {
+      document.documentElement.style.setProperty('--sk-section-metadata-table-background-color', '#123456');
+      expect(getPreferedBackgroundColor('Section Metadata')).to.equal('#123456');
+    });
+
+    it('returns the correct color for "metadata" block', () => {
+      document.documentElement.style.setProperty('--sk-metadata-table-background-color', '#654321');
+      expect(getPreferedBackgroundColor('Metadata')).to.equal('#654321');
+    });
+
+    it('returns the correct color for an unspecified block', () => {
+      document.documentElement.style.setProperty('--sk-block-table-background-color', '#abcdef');
+      expect(getPreferedBackgroundColor('Some Other Block')).to.equal('#abcdef');
+    });
+
+    it('falls back to default color if the CSS variable is not set', () => {
+      document.documentElement.style.removeProperty('--sk-section-metadata-table-background-color');
+      document.documentElement.style.removeProperty('--sk-metadata-table-background-color');
+      document.documentElement.style.removeProperty('--sk-block-table-background-color');
+      expect(getPreferedBackgroundColor('Section Metadata')).to.equal('#ff8012');
+    });
+  });
+
+  describe('getPreferedForegroundColor', () => {
+    it('returns the correct color for "section metadata" block', () => {
+      document.documentElement.style.setProperty('--sk-section-metadata-table-foreground-color', '#123456');
+      expect(getPreferedForegroundColor('Section Metadata')).to.equal('#123456');
+    });
+
+    it('returns the correct color for "metadata" block', () => {
+      document.documentElement.style.setProperty('--sk-metadata-table-foreground-color', '#654321');
+      expect(getPreferedForegroundColor('Metadata')).to.equal('#654321');
+    });
+
+    it('returns the correct color for an unspecified block', () => {
+      document.documentElement.style.setProperty('--sk-block-table-foreground-color', '#abcdef');
+      expect(getPreferedForegroundColor('Some Other Block')).to.equal('#abcdef');
+    });
+
+    it('falls back to default color if the CSS variable is not set', () => {
+      document.documentElement.style.removeProperty('--sk-section-metadata-table-foreground-color');
+      document.documentElement.style.removeProperty('--sk-metadata-table-foreground-color');
+      document.documentElement.style.removeProperty('--sk-block-table-foreground-color');
+      expect(getPreferedForegroundColor('Section Metadata')).to.equal('#ffffff');
     });
   });
 });
